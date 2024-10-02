4aaa0ac8bc8c448be086b1dc759dd9775980092e
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-10-02 15:40:23 +0300

    fix slideshow component if no photos found

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -5,12 +5,15 @@ export default function SlideShow({ photos }) {
   console.log(photos[0]);
   return (
     <div className="border border-black p-20">
-      <Image
-        src={photos[0]}
-        alt="Photo"
-        width={500}
-        height={500}
-      />
+      {photos.length === 0 && <p>No photos found, click Search</p>}
+      {photos.length === 1 && (
+        <Image
+          src={photos[0]}
+          alt="Photo"
+          width={500}
+          height={500}
+        />
+      )}
     </div>
   );
 }
