a91db4e5ea74beee2a2eaef3e118b3562a820894
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-10-17 14:02:15 +0300

    better console printing

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,21 +1,25 @@
 import Image from "next/image";
-const imageDisplayUrl = "https://www.finna.fi/Cover/Show?id="
 
 export default function SlideShow({ photos }) {
+  const imageDisplayUrl = "https://www.finna.fi/Cover/Show?id=";
+
   console.log("SlideShow component got:", photos);
+  if (photos.length === 0) {
+    return <p>No photos found, click Search</p>;
+  }
   const randomNumber = Math.floor(Math.random() * photos.length);
+  // TODO encode URL without manual hacks
+  const imageUrl =
+    imageDisplayUrl + photos[randomNumber].id.replaceAll("+", "%2B");
+  console.log("Displaying image:", imageUrl);
   return (
     <div className="border border-black p-20">
-      {photos.length === 0 && <p>No photos found, click Search</p>}
-      {photos.length > 0 && (
-        <Image
-          src={imageDisplayUrl + photos[randomNumber].id.replaceAll("+", "%2B")} 
-          // TODO encode URL without manual hacks
-          alt="Photo"
-          width={500}
-          height={500}
-        />
-      )}
+      <Image
+        src={imageUrl}
+        alt="Photo"
+        width={500}
+        height={500}
+      />
     </div>
   );
 }
--- src/services/photos.js
+++ src/services/photos.js
@@ -1,11 +1,13 @@
 const getPhotos = async () => {
   const apiUrl = "https://api.finna.fi/v1/search?filter[]=format:0/Image/&limit=50&page=3&field[]=title&field[]=images&field[]=id"
-    try {
+    try { 
         const response = await fetch(apiUrl)
+        console.log("Fetching by url", apiUrl)
         if (!response.ok) {
             throw new Error('Network response was not ok')
         }
         const data = await response.json()
+        console.log("data.records:,", data.records)
         return data.records
     } catch (error) {
         console.error('Error fetching photos:', error)
