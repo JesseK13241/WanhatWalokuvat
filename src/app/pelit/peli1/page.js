c5bd35336a2b8f6291a1e1a9948ac3fe36629c86
Niskanen Petja <petja.u.t.niskanen@student.jyu.fi>
2024-10-25 16:36:20 +0300

    Peli 1 runko, closes #31

--- src/app/pelit/peli1/page.js
+++ src/app/pelit/peli1/page.js
@@ -1,7 +1,38 @@
+import Image from "next/image";
+
 export default function Peli1() {
   return (
-    <div>
-      <h1>Peli 1 tällä sivulla</h1>
+    <div className="flex flex-col item-center px-10 text-center">
+      <h1 className="font-bold m-4">
+        Peli 1
+      </h1>
+      <div className="flex justify-center">
+        <Image
+          src={"https://api.finna.fi/Cover/Show?source=Solr&id=sls.SLSA%2B367_SLSA%2B367%253A5%253A5%253A5.1&index=0&size=large"}
+          alt={"Albert Edelfelt"}
+          height={500}
+          width={500}
+        />
+      </div>
+
+      <div className="grid grid-cols-2 gap-4 m-4">
+        <button
+        className="px-4 py-2 border rounded hover:bg-gray-100">
+          Option 1
+        </button>
+        <button
+        className="px-4 py-2 border rounded hover:bg-gray-100">
+          Option 2
+        </button>
+        <button
+        className="px-4 py-2 border rounded hover:bg-gray-100">
+          Option 3
+        </button>
+        <button
+        className="px-4 py-2 border rounded hover:bg-gray-100">
+          Option 4
+        </button>
+      </div>
     </div>
   );
 }
