7545af97660d220c78439222e6847c46670b9bfc
Sofia Palola <sofia.k.palola@student.jyu.fi>
2024-10-29 17:30:16 +0200

    DecadePicker muutettu dynaamiseksi

--- src/components/DecadePicker.js
+++ src/components/DecadePicker.js
@@ -1,6 +1,15 @@
 // TODO refactor, dynamic decade options
+import React from "react";
 
 export default function DecadePicker({ value, onChange }) {
+  const startYear = 1880;
+  const currentYear = new Date().getFullYear();
+  const endYear = currentYear - (currentYear % 10);
+
+  const decades = [];
+  for (let year = startYear; year <= endYear; year += 10) {
+    decades.push({label: `${year}s`, value: `${year}-${year + 9}`});
+  }
   return (
     <div>
       <label>Select decade</label>
@@ -11,21 +20,11 @@ export default function DecadePicker({ value, onChange }) {
           onChange(e.target.value);
         }} 
       >
-        <option value="1880-1889">1880s</option>  */
-        <option value="1890-1899">1890s</option>
-        <option value="1900-1909">1900s</option>
-        <option value="1910-1919">1910s</option>
-        <option value="1920-1929">1920s</option>
-        <option value="1930-1939">1930s</option>
-        <option value="1940-1949">1940s</option>
-        <option value="1950-1959">1950s</option>
-        <option value="1960-1969">1960s</option>
-        <option value="1970-1979">1970s</option>
-        <option value="1980-1989">1980s</option>
-        <option value="1990-1999">1990s</option>
-        <option value="2000-2009">2000s</option>
-        <option value="2010-2019">2010s</option>
-        <option value="2020-2029">2020s</option>
+        {decades.map((decade, index) => (
+          <option key={index} value={decade.value}>
+            {decade.label}
+          </option>
+        ))}
       </select>
     </div>
   );
