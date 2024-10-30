d56a5badb2c91791ccb2487fb957ddfb0c98f8b0
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-10-30 22:33:00 +0200

    Parempi footer, closes #29

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <footer className="flex border border-black p-5">
+    <footer className="flex border p-5">
       <Link
         href="https://github.com/JesseK13241/TIEA207-projekti/"
-        className="flex w-auto items-center gap-2 rounded border border-black bg-slate-300 p-2"
+        className="flex w-auto items-center gap-2 p-2"
       >
         <Image
           src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
