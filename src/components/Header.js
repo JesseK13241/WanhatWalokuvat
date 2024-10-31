e6c881dd94d6703811fe94a2306a6feb1e85637f
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-10-31 14:53:35 +0200

    Globaalit värit, closes #40

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
+import "@/app/globals.css"
 import Link from "next/link"
 
 const Header = () => {
   return (
-    <header className="border border-black p-5">
+    <header className="bg-maincolor border border-black p-5">
       <h1 className="border border-black text-3xl">
         <a href="/">Aineopintojen projektityö</a>
       </h1>
