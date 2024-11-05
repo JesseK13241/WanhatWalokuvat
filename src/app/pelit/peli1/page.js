d74a7877f6c0d25b7a922ae21c8b8b7ca87a28e3
Niskanen Petja <petja.u.t.niskanen@student.jyu.fi>
2024-11-05 20:30:08 +0200

    Peli toimii alkeellisesti, closes #46

--- src/app/pelit/peli1/page.js
+++ src/app/pelit/peli1/page.js
@@ -26,43 +26,53 @@ export default function Peli1() {
     setLoading(false)
   }
 
-  const answers = [
-    { decade: 1950, isCorrect: false },
-    { decade: 1970, isCorrect: true  },
-    { decade: 1990, isCorrect: false },
-    { decade: 2010, isCorrect: false }
-  ]
-
   if (isLoading) return <p>Loading...</p>
-  const imageUrl = "https://www.finna.fi/Cover/Show?id=" + encodeURIComponent(currentPhoto.id)
+
+  const imageUrl = currentPhoto ? "https://www.finna.fi/Cover/Show?id=" + encodeURIComponent(currentPhoto.id) : null
+
+  currentPhoto.author = Object.keys(currentPhoto.authors.primary)[0]
+  currentPhoto.building = currentPhoto.buildings[0].translated
 
   return (
-    <div className="flex flex-col text-center">
-      <h1 className="m-4 font-bold">Peli 1</h1>
-      <div className="flex justify-center"> 
+    <div className="flex flex-col items-center space-y-4">
+      <h1 className="mt-4 font-bold">Peli 1</h1>
+      <div> 
         <Image
             src={imageUrl}
             alt={currentPhoto.title}
-            width={500}
-            height={500}
+            width={0}
+            height={0}
             sizes="100vw"
+            className="h-80 w-auto object-cover"
             priority
-          />
+        />
       </div>
      
-      <div className="flex justify-center">
+      <div>
         <MultipleChoiceButtons 
-            answers={answers}
+            correctYear={currentPhoto.year}
         />
       </div>
 
-      <div className="flex justify-center">
-        <button
-          className="btn-primary"
-          onClick={nextRound}>
-            Uusi kierros
-        </button>
-      </div>
+      <button
+        className="btn-primary"
+        onClick={nextRound}>
+          Uusi kierros
+      </button>
+
+      <div className="mx-4 mb-4 text-center">
+        {currentPhoto.location && <p> Location: {currentPhoto.location} </p>}
+        {currentPhoto.title && <p> Title: {currentPhoto.title} </p>}
+        {currentPhoto.author && <p> Author: {currentPhoto.author} </p>}
+        {currentPhoto.building && <p> Building: {currentPhoto.building} </p>}
+        <a
+          href={"https://www.finna.fi" + currentPhoto.recordPage}
+          className="text-blue-600 visited:text-purple-600 hover:underline"
+        >
+          Linkki aineistoon
+        </a>
+      </div> 
+      
     </div>
   )
 }
--- src/components/MultipleChoiceButtons.js
+++ src/components/MultipleChoiceButtons.js
@@ -1,14 +1,45 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export default function MultipleChoiceButtons({ answers }) {
+export default function MultipleChoiceButtons({ correctYear }) {
   const [currentAnswer, setCurrentAnswer] = useState(0)
+  const [answers, setAnswers] = useState()
+
+  useEffect(() => {
+    generateAnswers()
+  }, [])
+
+  const generateAnswers = () => {
+    var newAnswers = [{}, {}, {}, {}]
+    const correctDecade = (Math.floor(correctYear / 10)) * 10
+
+    var possibleAnswers = []
+    for (let i = correctDecade - 50; i < correctDecade + 50; i += 10) {
+      if (i != correctDecade) possibleAnswers.push(i)
+    }
+
+    newAnswers[0].decade = correctDecade
+    newAnswers[0].isCorrect = true
+    
+    for (let i = 1; i < 4; i++) {
+      let j = Math.floor(Math.random() * possibleAnswers.length)
+      let randomDecade = possibleAnswers[j]
+      possibleAnswers.splice(j, 1)
+      newAnswers[i].decade = randomDecade
+      newAnswers[i].isCorrect = false
+    }
+
+    newAnswers.sort((a, b) => a.decade > b.decade)
+    setAnswers(newAnswers)
+  }
 
   const styles = {
     default: "btn-secondary",
     correct: "btn-secondary hover:bg-green-100 bg-green-100",
     incorrect: "btn-secondary hover:bg-red-100 bg-red-100",
   }
+
+  if (!answers) return <p>Loading..</p>
 
   answers.forEach((a) => {
     if (currentAnswer && a.isCorrect) a.style = "correct"
@@ -17,7 +48,7 @@ export default function MultipleChoiceButtons({ answers }) {
   })
 
   return (
-    <div className="m-4 grid grid-cols-2 gap-4">
+    <div className="flex justify-between space-x-4">
       {answers.map((answer) => (
         <button
           disabled={currentAnswer}
