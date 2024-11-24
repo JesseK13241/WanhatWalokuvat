473a45bf5da90daa93829bc9583189e56d16df16
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-11-24 10:38:08 +0200

    Basic LocationPicker with static coordinates

--- src/components/LocationPicker.js
+++ src/components/LocationPicker.js
@@ -1,3 +1,15 @@
-export default function LocationPicker() {
-  return <button className="btn-secondary">Sijainti</button>
+export default function LocationPicker({ value, onChange }) {
+  return (
+    <button
+      className="btn-secondary"
+      onClick={(e) => {
+        const coordinates = {"lat": 60, "lon": 25, "r": 10}
+        console.log("Changed location to:", coordinates)
+        onChange(coordinates)
+      }}
+    >
+      Sijainti
+    </button>
+  )
 }
+
