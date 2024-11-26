bf08e733326b8b49d590ac36bbd252f4e5b90d98
Sofia Palola <sofia.k.palola@student.jyu.fi>
2024-11-26 18:57:31 +0200

    Pelien etusivulle ruutu pelkän linkin sijaan

--- src/app/pelit/page.js
+++ src/app/pelit/page.js
@@ -1,8 +1,30 @@
 import Link from "next/link"
+import Image from "next/image"
+import arvaa from "../../public/images/arvaa.jpg"
+
 export default function Pelit() {
   return (
-    <div>
-      <Link href="/pelit/peli1">Peli 1 - linkki</Link>
+    <div className="flex flex-wrap gap-4">
+      <div className="border p-4 text-center flex flex-col justify-center items-center">
+        <Link href="/pelit/peli1">
+          <div className="flex flex-col justify-center items-center">
+            <h1>Arvaa vuosikymmen</h1>
+            <Image
+              src={arvaa}
+              alt="Arvaa vuosikymmen"
+              width={400}
+              height={200}
+            />
+            <p>Arvaa neljästä vaihtoehdosta, miltä vuosikymmeneltä kuva on</p>
+          </div>
+        </Link>
+      </div>
+      <div className="border p-4 text-center">
+        <div>
+          <h2>Peli 2</h2>
+          <p>Tähän vois laittaa toisen pelin</p>
+        </div>
+      </div>
     </div>
   )
 }
