ee5b18e05f88f4206120efad52d2fe9046e8680a
eliaslehtinen <elias.a.lehtinen@gmail.com>
2024-11-29 21:41:13 +0200

    Pelin 2 runko

--- src/app/pelit/peli2/page.js
+++ src/app/pelit/peli2/page.js
@@ -2,11 +2,14 @@
 import getRandomPhoto from "@/services/photos"
 import { useEffect, useState } from "react"
 import PhotoContainer from "@/components/PhotoContainer"
+import PhotoInfoContainer from "@/components/PhotoInfoContainer"
+import PhotoContainerSkeleton from "@/components/PhotoContainerSkeleton"
 
 // Peli, jossa pelaaja arvaa kumpi kuva on vanhempi
 export default function Peli2({ decadeRange }) {
   const [leftPhoto, setLeftPhoto] = useState(null)
   const [rightPhoto, setRightPhoto] = useState(null)
+  const [answered, setAnswered] = useState(false)
 
   useEffect(() => {
     getRandomPhoto({
@@ -23,19 +26,57 @@ export default function Peli2({ decadeRange }) {
   }, [])
 
   if (leftPhoto == null || rightPhoto == null) {
-    return <div>Loading...</div>
+    return (
+      <div>
+        <div className="flex justify-center text-xl">
+          <p className="mt-4 rounded-xl bg-tertiary p-4">
+            Klikkaa vanhempaa kuvaa
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-center p-4 pb-14">
+          <PhotoContainerSkeleton />
+          <PhotoContainerSkeleton />
+        </div>
+      </div>
+    )
   } else {
     console.log("left", leftPhoto)
     console.log("right", rightPhoto)
   }
 
   return (
-    <div className="flex">
-      <div>
-        <PhotoContainer photo={leftPhoto} />
+    <div>
+      <div className="flex justify-center text-xl">
+        <p className="mt-4 rounded-xl bg-tertiary p-4">
+          Klikkaa vanhempaa kuvaa
+        </p>
       </div>
-      <div>
-        <PhotoContainer photo={rightPhoto} />
+      <div className="flex flex-wrap justify-center p-4 pb-14">
+        <div>
+          {leftPhoto ? (
+            <PhotoContainer
+              photo={leftPhoto}
+              infoElem={
+                <PhotoInfoContainer
+                  photo={leftPhoto.records[0]}
+                  showTitle={answered}
+                />
+              }
+            />
+          ) : (
+            <PhotoContainerSkeleton />
+          )}
+        </div>
+        <div>
+          {rightPhoto && (
+            <PhotoContainer
+              photo={rightPhoto}
+              infoElem={
+                <PhotoInfoContainer photo={rightPhoto} showTitle={answered} />
+              }
+            />
+          )}
+        </div>
       </div>
     </div>
   )
