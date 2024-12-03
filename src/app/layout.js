73f741ae1bcb7b07493b176a62606c169e1b6045
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-12-03 08:18:57 +0200

    header ja footer app-folderiin

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
+import Footer from "@/app/Footer"
+import Header from "@/app/Header"
 import "./globals.css"
 
 export const metadata = {
