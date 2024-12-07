c6fd1125d19ef052dd176010e65b040eacdc190c
Jesse K <138243431+JesseK13241@users.noreply.github.com>
2024-12-07 12:57:05 +0200

    parempi footer, closes #29

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,19 +1,15 @@
-import Image from "next/image"
-import Link from "next/link"
+import Link from "next/link";
+
+import { Github } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="flex bg-secondary p-5">
+    <footer className="flex bg-secondary p-5 shadow-inner">
       <Link
         href="https://github.com/JesseK13241/TIEA207-projekti/"
-        className="flex w-auto items-center gap-2 p-2"
+        className="flex w-auto items-center gap-2 p-3 border border-black rounded-lg bg-inherit hover:saturate-200"
       >
-        <Image
-          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-          alt="GitHub Logo"
-          width={30}
-          height={30}
-        />
+        <Github/>
         View on GitHub
       </Link>
     </footer>
