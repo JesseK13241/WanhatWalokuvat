9acefc8272c07bba8c52c0a486b4a0cc384040ed
Niskanen Petja <petja.u.t.niskanen@student.jyu.fi>
2024-12-10 10:04:49 +0200

    Toggle korjaus

--- src/components/ToggleInput.js
+++ src/components/ToggleInput.js
@@ -12,14 +12,14 @@ export default function ToggleInput({ labelText, value, setValue }) {
   return (
     <div className="flex w-64 justify-between font-bold">
       {labelText}
-      <label className="relative mx-2 block h-8 w-14 border-2 border-accent cursor-pointer rounded-full bg-primary">
+      <label className="relative mx-2 block h-8 w-14 cursor-pointer rounded-full bg-primary">
         <input
           defaultChecked={value}
           type="checkbox"
           className="peer sr-only"
           onChange={() => setValue(!value)}
         />
-        <span className="absolute left-0.5 top-0.5 size-6 rounded-full bg-secondary peer-checked:left-7 peer-checked:bg-accent" />
+        <span className="absolute left-1 top-1 size-6 rounded-full bg-secondary peer-checked:left-7 peer-checked:bg-accent" />
       </label>
     </div>
   )
